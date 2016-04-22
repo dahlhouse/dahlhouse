efe64ac2c075d68fc7845c4b58388a142d97b408
Matthew Dahl <dahl.matthew@gmail.com>
2016-04-21 22:00:29 -0700

    Minor cleanup and adding ember-frost-core

--- config/environment.js
+++ config/environment.js
@@ -1,46 +1,33 @@
-/* jshint node: true */
-
 module.exports = function (environment) {
   var ENV = {
-    modulePrefix: 'dahlhouse',
-    environment: environment,
+    APP: {},
     baseURL: '/',
-    locationType: 'auto',
     EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
-      }
+      FEATURES: {}
     },
-
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
-    }
+    environment: environment,
+    locationType: 'auto',
+    modulePrefix: 'dahlhouse'
   }
 
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
-  }
+  switch (environment) {
+    case 'development':
+      break
 
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.baseURL = '/'
-    ENV.locationType = 'none'
+    case 'production':
+      break
 
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false
-    ENV.APP.LOG_VIEW_LOOKUPS = false
-
-    ENV.APP.rootElement = '#ember-testing'
-  }
-
-  if (environment === 'production') {
-
+    case 'test':
+      Object.assign(ENV, {
+        baseUrl: '/',
+        locationType: 'none'
+      })
+      Object.assign(ENV.APP, {
+        LOG_ACTIVE_GENERATION: false,
+        LOG_VIEW_LOOKUPS: false,
+        rootElement: '#ember-testing'
+      })
+      break
   }
 
   return ENV
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,8 +3,16 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app')
 
 module.exports = function (defaults) {
   var app = new EmberApp(defaults, {
+    babel: {
+      optional: ['es7.decorators']
+    },
     'ember-cli-mocha': {
       useLintTree: false
+    },
+    sassOptions: {
+      includePaths: [
+        'node_modules/ember-frost-core/addon/styles'
+      ]
     }
   })
 
